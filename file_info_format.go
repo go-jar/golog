5e14badf900d38bb243336ad1967f0812ca4d3f4
go-jar <18800110390@163.com>
2020-11-01 20:53:06 +0800

    添加新的 log 格式

---
+++ file_info_format.go
@@ -0,0 +1,39 @@
+package golog
+
+import (
+	"path"
+	"runtime"
+	"strconv"
+	"time"
+
+	"github.com/goinbox/gomisc"
+)
+
+type FileInfoFormat struct {
+	timePattern string
+}
+
+func NewFileInfoFormat() *FileInfoFormat {
+	return &FileInfoFormat{
+		timePattern: "2006-01-02 15:04:05",
+	}
+}
+
+func (sf *FileInfoFormat) SetTimePattern(tp string) {
+	sf.timePattern = tp
+}
+
+func (sf *FileInfoFormat) Format(level int, msg []byte) []byte {
+	logLevel, ok := LogLevel[level]
+	if !ok {
+		logLevel = []byte("-")
+	}
+
+	_, fullFileName, line, _ := runtime.Caller(0)
+	fileName := path.Base(fullFileName)
+
+	return gomisc.AppendBytes([]byte("["), logLevel, []byte("]\t["),
+		[]byte(time.Now().Format(sf.timePattern)), []byte("]\t["),
+		[]byte(fileName), []byte(": "), []byte(strconv.Itoa(line)), []byte("] "),
+		msg, []byte("\n"))
+}
--- format_test.go
+++ format_test.go
@@ -9,4 +9,8 @@ func TestConsoleFormat(t *testing.T) {
 	sf := NewSimpleFormat()
 	cf := NewConsoleFormat(sf)
 	fmt.Println(string(cf.Format(LEVEL_INFO, []byte("Hello, World!"))))
+
+	fif := NewFileInfoFormat()
+	cf = NewConsoleFormat(fif)
+	fmt.Println(string(cf.Format(LEVEL_DEBUG, []byte("Hello, World!"))))
 }
